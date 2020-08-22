e2ce1a96b54075c3b0f256d919195ea65a62bf69
Konstantin Tenman <ktenman@users.noreply.github.com>
2020-08-22 07:50:07 +0100

    initial commit

---
+++ src/main/java/ee/tenman/booker/BookerApplication.java
@@ -0,0 +1,15 @@
+package ee.tenman.booker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class BookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ee/tenman/booker/BookingService.java
@@ -0,0 +1,118 @@
+package ee.tenman.booker;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.closeWindow;
+import static com.codeborne.selenide.Selenide.open;
+
+@Service
+public class BookingService {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd/MM");
+
+    @Value("${email}")
+    private String email;
+
+    @Value("${password}")
+    private String password;
+
+    @Scheduled(cron = "00 59 17 * * ?")
+    public void register() throws InterruptedException {
+        long start = System.nanoTime();
+        Configuration.headless = true;
+
+        login();
+        selectSwimmingActivity();
+        waitUntil6Pm();
+
+        if (!hasRegistered()) {
+            log.info("Not registered.");
+            tearDown(start);
+            return;
+        }
+        agreeToBookingTerms();
+        tearDown(start);
+    }
+
+    private boolean hasRegistered() {
+        open("https://better.legendonlineservices.co.uk/poplar_baths/BookingsCentre/Timetable?KeepThis=true&#");
+        LocalDateTime localDateTime = LocalDateTime.now().plusDays(7);
+        for (SelenideElement e : $(By.tagName("table")).$$("tr")) {
+            String text = e.getText();
+            if (text.contains("London") && text.contains(localDateTime.format(DATE_TIME_FORMATTER)) && text.contains("06:30") && text.contains("06:40")) {
+                e.$$(By.tagName("a")).find(text("Add to Basket")).click();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void tearDown(long start) {
+        log.info("Finishing with: {}s", duration(start, System.nanoTime()));
+        closeWindow();
+        closeWebDriver();
+    }
+
+    private void agreeToBookingTerms() {
+        open("https://better.legendonlineservices.co.uk/poplar_baths/Basket/Index");
+        $(By.id("agreeBookingTerms")).click();
+        $(By.id("btnPayNow")).click();
+    }
+
+    private void waitUntil6Pm() {
+        boolean isWait = true;
+        while (isWait) {
+            boolean isInPast = LocalDateTime.now()
+                    .isAfter(LocalDateTime.of(LocalDate.now(), LocalDateTime.parse("2015-08-04T18:00:00").toLocalTime()));
+            if (isInPast) {
+                log.info("Continue working!");
+                isWait = false;
+            }
+            try {
+                TimeUnit.MILLISECONDS.sleep(100);
+            } catch (Exception ignored) {
+            }
+        }
+    }
+
+    private void selectSwimmingActivity() throws InterruptedException {
+        open("https://better.legendonlineservices.co.uk/poplar_baths/BookingsCentre/Index");
+        $(By.linkText("Hackney")).click();
+        $(By.className("cscLeftPane")).$$(By.className("clubResult")).find(text("London Fields Lido")).$(By.tagName("label")).click();
+        $(By.linkText("Tower Hamlets")).click();
+        $(By.className("cscLeftPane")).$$(By.className("clubResult")).find(text("Poplar Baths LC")).$(By.tagName("label")).click();
+        TimeUnit.MILLISECONDS.sleep(100);
+        $(By.id("behaviours")).$$(By.className("activityItem")).find(text("Swim")).$(By.tagName("label")).click();
+        TimeUnit.MILLISECONDS.sleep(100);
+        $(By.id("activities")).$$(By.className("activityItem")).find(text("Swim for Fitness")).$(By.tagName("label")).click();
+        $(By.id("bottomsubmit")).click();
+    }
+
+    private void login() {
+        open("https://better.legendonlineservices.co.uk/enterprise/account/login");
+        $(By.id("login_Email")).setValue(email);
+        $(By.id("login_Password")).setValue(password);
+        $(By.id("login")).click();
+    }
+
+    public double duration(long start, long finish) {
+        return (finish - start) / 1_000_000_000.0;
+    }
+
+}
