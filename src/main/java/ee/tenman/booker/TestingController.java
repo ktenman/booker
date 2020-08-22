8b3daf51b994a5f1ed3bcbb0b137b6808365100d
Konstantin Tenman <ktenman@users.noreply.github.com>
2020-08-22 12:33:02 +0100

    refactor constructor

--- src/main/java/ee/tenman/booker/TestingController.java
+++ src/main/java/ee/tenman/booker/TestingController.java
@@ -3,16 +3,18 @@ package ee.tenman.booker;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 import java.util.Map;
 
 @RestController
 public class TestingController {
 
-    @Resource
-    private BookingService bookingService;
+    private final BookingService bookingService;
 
-    @GetMapping("test")
+    public TestingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    @GetMapping("/test")
     public Map<String, Boolean> test() {
         return bookingService.login();
     }
